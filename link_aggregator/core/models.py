f5b35499ed28897a62025d03f6641d907f55ba0a
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-09-18 12:33:36 -0300

    adiconado Link model

--- link_aggregator/core/models.py
+++ link_aggregator/core/models.py
@@ -1,3 +1,22 @@
+import uuid as uuid_lib
+
 from django.db import models
 
-# Create your models here.
+
+class DefaultBaseModel(models.Model):
+    created_at = models.DateTimeField("criado em", auto_now_add=True)
+    updated_at = models.DateTimeField("atualizado em", auto_now=True)
+    uuid = models.UUIDField(
+        "UUID", unique=True, editable=False, default=uuid_lib.uuid4, db_index=True
+    )
+
+    class Meta:
+        abstract = True
+
+
+class Link(DefaultBaseModel):
+    url = models.URLField("URL", unique=True)
+    description = models.TextField("descrição", blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.url
