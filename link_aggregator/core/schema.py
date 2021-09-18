999dd96f6c3b2fdde75bf18bc1536f76fa9e1867
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-09-18 16:21:04 -0300

    usando método relay para filtrar os links

--- link_aggregator/core/schema.py
+++ link_aggregator/core/schema.py
@@ -1,16 +1,23 @@
+import django_filters
 import graphene
 from graphene_django import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Link
 
 
-class LinkType(DjangoObjectType):
+class LinkFilter(django_filters.FilterSet):
     class Meta:
         model = Link
+        fields = ["url", "description"]
+
+
+class LinkNode(DjangoObjectType):
+    class Meta:
+        model = Link
+        interfaces = (graphene.relay.Node,)
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
-
-    def resolve_links(self, info, **kwargs):
-        return Link.objects.order_by("-created_at")
+    link = graphene.relay.Node.Field(LinkNode)
+    links = DjangoFilterConnectionField(LinkNode, filterset_class=LinkFilter)
