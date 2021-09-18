4d5a5127dfbc0a1c1a5a44c4ec56db4da5954ec5
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-09-18 16:12:04 -0300

    filtrando registros de usuário

--- link_aggregator/users/schema.py
+++ link_aggregator/users/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from graphene_django import DjangoObjectType
 from graphql_jwt.decorators import login_required
 
@@ -10,8 +11,16 @@ class UserType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    users = graphene.List(UserType)
+    users = graphene.List(UserType, q=graphene.String())
 
     @login_required
-    def resolve_users(self, info):
-        return get_user_model().objects.all()
+    def resolve_users(self, info, **kwargs):
+        qs = get_user_model().objects.all()
+        if kwargs.get("q"):
+            q = kwargs.get("q")
+            qs = qs.filter(
+                Q(username__icontains=q)
+                | Q(first_name__icontains=q)
+                | Q(email__icontains=q)
+            )
+        return qs
