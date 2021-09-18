00e1263a9e83e91ec3ee0861c9e59ef1bf699eb0
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-09-18 12:09:30 -0300

    adicionado python decouple

---
+++ contrib/generate_env.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from django.core.management.utils import get_random_secret_key
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+ENV_VARIABLES = f"""SECRET_KEY={get_random_secret_key()}
+DEBUG=True
+ALLOWED_HOSTS=*
+"""
+
+file = open(f"{BASE_DIR}/link_aggregator/.env", "w")
+file.write(ENV_VARIABLES)
--- link_aggregator/settings.py
+++ link_aggregator/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import Csv, config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-77d-hpt$5h5)ie1$8y9snjqe$+u3@6%zj#2y!iyt&xtp%-t@f*"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 
 # Application definition
